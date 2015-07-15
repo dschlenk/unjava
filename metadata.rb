d7cd522f9b43d7adb048c275fd2084f59150207b
David Schlenk <david.schlenk@spanlink.com>
2015-07-15 16:47:19 -0500

    Bump version for dev; improve metadata

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,23 @@ maintainer_email 'dschlenk@converge-one.com'
 license 'apache2'
 description 'Installs/Configures unjava'
 long_description 'Installs/Configures unjava'
-version '0.1.0'
+version '0.1.1'
 depends 'java'
+
+recipe "unjava::default", "Includes unjava::unjava."
+recipe "unjava::unjava", "Prevents installation of OpenJDK due to the inclusion of the default community cookbook in another recipe in the run list."
+recipe "unjava::rejava", "Reverses the prevention of OpenJDK installation from the unjava recipe and includes the default recipe from the community java cookbook."
+
+%w{
+    debian
+    ubuntu
+    centos
+    redhat
+    scientific
+    fedora
+    amazon
+    oracle
+    freebsd
+}.each do |os|
+  supports os
+end
